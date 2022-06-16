9852811c089481d3e4db4e2e13d274da76b69c60
nurlan <nurlan.arapov9898@gmail.com>
2022-06-16 23:34:23 +0600

    RabbitStart Project done

--- RabbitConsumer/Program.cs
+++ RabbitConsumer/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Consumer");
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+/// <summary>
+/// Получение простого сообщения из очереди hello
+/// </summary>
+public class Program
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine($"Получение сообщение {message}");
+            };
+
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
--- RabbitStart/Program.cs
+++ RabbitStart/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Publisher");
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+/// <summary>
+/// Отправка простого сообщения в очередь hello
+/// </summary>
+public class Program
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "Hello",
+                                 durable: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            string message = "Started RabbitMq";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+
+            Console.WriteLine($"Отправлено сообщение: {message}");
+        }
+        Console.ReadLine();
+    }
+}
