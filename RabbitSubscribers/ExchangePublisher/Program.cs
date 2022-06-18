84f20480ac2a1354a025dc33244769f1797c2f49
nurlan <nurlan.arapov9898@gmail.com>
2022-06-18 16:18:23 +0600

    RabbitExchange Subscribers created

---
+++ RabbitSubscribers/ExchangePublisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+public class Program
+{
+    public static void Main()
+    {
+        int i = 0;
+        do
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("Logs", type: ExchangeType.Fanout);
+
+                var message = $"Message from exchange {i}";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "Logs", routingKey: "", basicProperties: null
+                                     , body: body);
+
+                Console.WriteLine($"Sended message: {message}");
+                Thread.Sleep(1000);
+            }
+            i++;
+        }
+        while (true);      
+    }
+}
---
+++ RabbitSubscribers/RabbitSubscriberLogs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class Program
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare("Logs", type: ExchangeType.Fanout);
+
+            var QueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queue: QueName, exchange: "Logs",
+                             routingKey: "");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+                
+            };
+
+            channel.BasicConsume(queue: QueName, autoAck: true, consumer: consumer);
+            Console.ReadLine();
+        }
+        
+    }
+}
