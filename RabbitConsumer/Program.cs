77da1f929e0299a5a67152543b42e474d272727e
nurlan <nurlan.arapov9898@gmail.com>
2022-06-18 15:32:45 +0600

    rabbit worker queues created

---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class Program
+{
+    /// <summary>
+    /// Получает сообщение на выполнение
+    /// </summary>
+    public static void Main()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+           
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Thread.Sleep(80);
+                Console.WriteLine($"Получение сообщение {message}");
+            };
+
+            channel.BasicConsume(queue: "TaskQueue",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitPublisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+public class Program
+{
+    /// <summary>
+    /// Посылает сообщение работникам на выполнение и автоматически удаляется
+    /// </summary>
+    public static void Main()
+    {
+        int i = 1;
+        do
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var message = $"Task {i}";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "TaskQueue",
+                                     basicProperties: properties,
+                                     body: body);
+
+                Console.WriteLine($"Отправлено сообщение: {message}");
+            }
+            i++;
+            Thread.Sleep(60);
+        }
+        while (true);
+        
+    }
+}
